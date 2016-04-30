707243c561f06ee063c5bf10107e4c7db5748fb6
Yu Liang <109827298@qq.com>
2016-04-30 10:20:43 +0800

    提交1.py
    提交1.py

---
+++ LiaoXueFeng/1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+'''
+age = 20
+if age >= 18:
+	print 'your age is', age
+	print 'adult'
+'''
+
+'''
+age = 3
+if age >= 18:
+	print 'your age is',age
+	print 'adult'
+else:
+	print 'your age is',age
+	print 'teenager'
+'''
+
+'''
+x = ()
+if x:
+	print 'True'
+'''
