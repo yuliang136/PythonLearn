262b9ab64873169a9818762f213f6287ceddbb98
yuliang <yuliang@naocy.com>
2016-05-03 11:55:49 +0800

    上传yuliang53
    上传yuliang53

--- LiaoXueFeng/yuliang53.py
+++ LiaoXueFeng/yuliang53.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+
+def calc(*numbers):
+	sum = 0
+	for n in numbers:
+		sum = sum + n * n
+	return sum
+
+def person(name,age,**kw):
+	print 'name:',name, 'age:',age, 'other:',kw
+
+def func(a,b,c=0,*args,**kw):
+	print 'a =', a, 'b =', b, 'c =', c, 'args =', args, 'kw =', kw
